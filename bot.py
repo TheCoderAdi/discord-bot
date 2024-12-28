4d18163ad229f3f21b7607892ae5af00e7ff977f
Aditya <adityasahoo652@gmail.com>
2024-12-28 11:56:36 +0530

    feat: integrate dotenv for environment variable management deps: add python-dotenv to requirements

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import discord
 import requests
+from dotenv import load_dotenv
 import os
+
+load_dotenv()
 
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 PRODIA_API_KEY = os.getenv('PRODIA_API_KEY')
